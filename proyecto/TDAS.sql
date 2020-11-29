422723e08a016fa23c9b3d61d659557fedb85ab2
IxDevel <56054255+IxDevel@users.noreply.github.com>
2020-11-28 22:04:05 -0400

    minor fix

--- proyecto/TDAS.sql
+++ proyecto/TDAS.sql
@@ -1,35 +1,61 @@
-CREATE OR REPLACE TYPE rango_tiempo AS OBJECT (
-    fecha_inicio DATE,
-    fecha_fin DATE,
-    CONSTRUCTOR FUNCTION rango_tiempo(SELF IN OUT NOCOPY rango_tiempo) RETURN SELF AS RESULT,
-    CONSTRUCTOR FUNCTION rango_tiempo(SELF IN OUT NOCOPY rango_tiempo, fecha_inicio DATE) RETURN SELF AS RESULT,
-    CONSTRUCTOR FUNCTION rango_tiempo(SELF IN OUT NOCOPY rango_tiempo, fecha_inicio DATE,fecha_fin DATE) RETURN SELF AS RESULT,
-    STATIC FUNCTION valido(fecha_inicio DATE,fecha_fin DATE) RETURN BOOLEAN,
-    STATIC FUNCTION vencido(fecha_fin DATE) RETURN BOOLEAN,
-    STATIC FUNCTION tiempo_para_caducar(fecha_fin DATE, fecha_objetivo DATE) RETURN VARCHAR2,
-    STATIC FUNCTION tiempo_para_caducar(fecha_fin DATE) RETURN VARCHAR2
+-- CREATES TDAS
+CREATE OR REPLACE TYPE MARCA AS OBJECT(
+    nombre VARCHAR2(80),
+    rif VARCHAR2(18),
+    fecha_de_registro DATE,
+    CONSTRUCTOR FUNCTION MARCA(SELF IN OUT NOCOPY MARCA, nombre VARCHAR2, RIF VARCHAR2, fecha_de_registro DATE) RETURN SELF AS RESULT,
+    STATIC FUNCTION VALIDAR_RIF(rif VARCHAR2) RETURN VARCHAR2
 );
 /
+CREATE OR REPLACE TYPE BODY MARCA IS
+    STATIC FUNCTION VALIDAR_RIF(rif VARCHAR2) RETURN VARCHAR2 IS
+    BEGIN
+        IF REGEXP_LIKE(rif, '^J[[:digit:]]{8}-[[:digit:]]') THEN
+            RETURN rif;
+        ELSE
+            RAISE_APPLICATION_ERROR(-20001,'Error: El rif no es válido');
+        END IF;
+    END;
 
-CREATE OR REPLACE TYPE BODY rango_tiempo IS
-
-    CONSTRUCTOR FUNCTION rango_tiempo(SELF IN OUT NOCOPY rango_tiempo) RETURN SELF AS RESULT IS
+    CONSTRUCTOR FUNCTION MARCA(SELF IN OUT NOCOPY MARCA, nombre VARCHAR2, rif VARCHAR2, fecha_de_registro DATE) RETURN SELF AS RESULT IS
+    BEGIN
+        SELF.nombre := nombre;
+        SELF.rif := MARCA.VALIDAR_RIF(rif);
+        SELF.fecha_de_registro := fecha_de_registro;
+        RETURN;
+    END;
+END;
+/
+CREATE OR REPLACE TYPE RANGO_TIEMPO AS OBJECT (
+    fecha_inicio DATE,
+    fecha_fin DATE,
+    CONSTRUCTOR FUNCTION RANGO_TIEMPO(SELF IN OUT NOCOPY RANGO_TIEMPO) RETURN SELF AS RESULT,
+    CONSTRUCTOR FUNCTION RANGO_TIEMPO(SELF IN OUT NOCOPY RANGO_TIEMPO, fecha_inicio DATE) RETURN SELF AS RESULT,
+    CONSTRUCTOR FUNCTION RANGO_TIEMPO(SELF IN OUT NOCOPY RANGO_TIEMPO, fecha_inicio DATE,fecha_fin DATE) RETURN SELF AS RESULT,
+    STATIC FUNCTION VALIDO(fecha_inicio DATE,fecha_fin DATE) RETURN BOOLEAN,
+    STATIC FUNCTION VENCIDO(fecha_fin DATE) RETURN BOOLEAN,
+    STATIC FUNCTION TIEMPO_PARA_CADUCAR(fecha_fin DATE, fecha_objetivo DATE) RETURN VARCHAR2,
+    STATIC FUNCTION TIEMPO_PARA_CADUCAR(fecha_fin DATE) RETURN VARCHAR2
+);
+/
+CREATE OR REPLACE TYPE BODY RANGO_TIEMPO IS
+    CONSTRUCTOR FUNCTION RANGO_TIEMPO(SELF IN OUT NOCOPY rango_tiempo) RETURN SELF AS RESULT IS
     BEGIN
         SELF.fecha_inicio := NULL;
         SELF.fecha_fin := NULL;
         RETURN;
     END;
 
-    CONSTRUCTOR FUNCTION rango_tiempo(SELF IN OUT NOCOPY rango_tiempo,fecha_inicio DATE) RETURN SELF AS RESULT IS
+    CONSTRUCTOR FUNCTION RANGO_TIEMPO(SELF IN OUT NOCOPY rango_tiempo,fecha_inicio DATE) RETURN SELF AS RESULT IS
     BEGIN
         SELF.fecha_inicio := fecha_inicio;
         SELF.fecha_fin := NULL;
         RETURN;
     END;
 
-    CONSTRUCTOR FUNCTION rango_tiempo(SELF IN OUT NOCOPY rango_tiempo, fecha_inicio DATE,fecha_fin DATE) RETURN SELF AS RESULT IS
+    CONSTRUCTOR FUNCTION RANGO_TIEMPO(SELF IN OUT NOCOPY rango_tiempo, fecha_inicio DATE,fecha_fin DATE) RETURN SELF AS RESULT IS
     BEGIN
-        IF rango_tiempo.valido(fecha_inicio,fecha_fin) THEN
+        IF RANGO_TIEMPO.valido(fecha_inicio,fecha_fin) THEN
             SELF.fecha_inicio := fecha_inicio;
             SELF.fecha_fin := fecha_fin;
         ELSE
@@ -38,24 +64,24 @@ CREATE OR REPLACE TYPE BODY rango_tiempo IS
         RETURN;
     END;
 
-    STATIC FUNCTION valido(fecha_inicio DATE,fecha_fin DATE) RETURN BOOLEAN
+    STATIC FUNCTION VALIDO(fecha_inicio DATE,fecha_fin DATE) RETURN BOOLEAN
     IS
     BEGIN
         RETURN fecha_inicio <= fecha_fin;
     END;
 
-    STATIC FUNCTION vencido(fecha_fin DATE) RETURN BOOLEAN
+    STATIC FUNCTION VENCIDO(fecha_fin DATE) RETURN BOOLEAN
     IS
     BEGIN
         RETURN SYSDATE > fecha_fin;
     END;
 
-   STATIC FUNCTION tiempo_para_caducar(fecha_fin DATE, fecha_objetivo DATE) RETURN VARCHAR2
+   STATIC FUNCTION TIEMPO_PARA_CADUCAR(fecha_fin DATE, fecha_objetivo DATE) RETURN VARCHAR2
     AS
         d_diff NUMBER;
     BEGIN
         d_diff := fecha_fin - fecha_objetivo;
-        IF (NOT rango_tiempo.valido(fecha_objetivo,fecha_fin)) THEN RAISE_APPLICATION_ERROR(-20001,'Error: rango de fechas no válidos'); END IF;
+        IF (NOT RANGO_TIEMPO.valido(fecha_objetivo,fecha_fin)) THEN RAISE_APPLICATION_ERROR(-20001,'Error: rango de fechas no válidos'); END IF;
 
         IF (d_diff < 1) THEN
             RETURN REGEXP_SUBSTR(NUMTODSINTERVAL(d_diff,'DAY'),'\d{2}:\d{2}');
@@ -67,30 +93,56 @@ CREATE OR REPLACE TYPE BODY rango_tiempo IS
 
     END;
 
-    STATIC FUNCTION tiempo_para_caducar(fecha_fin DATE) RETURN VARCHAR2
+    STATIC FUNCTION TIEMPO_PARA_CADUCAR(fecha_fin DATE) RETURN VARCHAR2
     IS
     BEGIN
-        RETURN tiempo_para_caducar(fecha_fin,CURRENT_DATE);
+        RETURN TIEMPO_PARA_CADUCAR(fecha_fin,CURRENT_DATE);
     END;
 END;
 /
-
-CREATE OR REPLACE TYPE MARCA AS OBJECT(
-    nombre VARCHAR2(80),
-    rif VARCHAR2(11),
-    fecha_de_registro DATE,
-    --logo BLOB,
-    STATIC FUNCTION VALIDAR_RIF(rif VARCHAR2) RETURN VARCHAR2
+CREATE OR REPLACE TYPE CANCELACION AS OBJECT (
+   fecha_cancelacion DATE,
+   motivo VARCHAR(2000),
+   CONSTRUCTOR FUNCTION CANCELACION(SELF IN OUT NOCOPY CANCELACION, fecha_cancelacion DATE) RETURN SELF AS RESULT,
+   CONSTRUCTOR FUNCTION CANCELACION(SELF IN OUT NOCOPY CANCELACION, fecha_cancelacion DATE, motivo VARCHAR2) RETURN SELF AS RESULT,
+   STATIC FUNCTION VALIDAR_FINALIZACION(fecha_cancelacion DATE) RETURN DATE
 );
 /
-CREATE OR REPLACE TYPE BODY MARCA IS
-    STATIC FUNCTION VALIDAR_RIF(rif VARCHAR2) RETURN VARCHAR2 IS
+CREATE OR REPLACE TYPE BODY CANCELACION IS
+    CONSTRUCTOR FUNCTION CANCELACION(SELF IN OUT NOCOPY CANCELACION, fecha_cancelacion DATE) RETURN SELF AS RESULT IS
     BEGIN
-        IF True THEN
-            RETURN rif;
+        SELF.fecha_cancelacion := CANCELACION.VALIDAR_FINALIZACION(fecha_cancelacion);
+        SELF.motivo := NULL;
+        RETURN;
+    END;
+
+    CONSTRUCTOR FUNCTION CANCELACION(SELF IN OUT NOCOPY CANCELACION, fecha_cancelacion DATE, motivo VARCHAR2) RETURN SELF AS RESULT IS
+    BEGIN
+        SELF.fecha_cancelacion := CANCELACION.VALIDAR_FINALIZACION(fecha_cancelacion);
+        SELF.motivo := motivo;
+        RETURN;
+    END;
+
+    STATIC FUNCTION VALIDAR_FINALIZACION(fecha_cancelacion DATE) RETURN DATE IS
+    BEGIN
+        IF fecha_cancelacion > SYSDATE THEN
+            RETURN fecha_cancelacion;
         ELSE
-            RAISE_APPLICATION_ERROR(-20001,'Error: El rif no es válido');
+            RAISE_APPLICATION_ERROR(-20001,'Error: la fecha de caducidad no puede ser menor a la actual');
         END IF;
+    END;
+END;
+/
+CREATE OR REPLACE TYPE UBICACION AS OBJECT (
+    coordenada SDO_GEOMETRY,
+    actualizado TIMESTAMP,
+    MEMBER FUNCTION OBTENER_POSICION(timpo_transcurrido TIMESTAMP, velocidad_med NUMBER) RETURN SDO_GEOMETRY
+);
+/
+CREATE OR REPLACE TYPE BODY UBICACION IS
+    MEMBER FUNCTION OBTENER_POSICION(timpo_transcurrido TIMESTAMP, velocidad_med NUMBER) RETURN SDO_GEOMETRY IS
+    BEGIN
+        RETURN NULL;
     END;
 END;
 /
