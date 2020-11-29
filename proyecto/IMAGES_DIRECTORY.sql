a5bcf2e67fcc4e095fa0cb4de8e05734fc586e84
IxDevel <56054255+IxDevel@users.noreply.github.com>
2020-11-28 22:39:17 -0400

    LAB5

---
+++ proyecto/IMAGES_DIRECTORY.sql
@@ -0,0 +1,4 @@
+-- Primero deben crear una carpeta para las imágenes y colocar la ruta después del AS
+CREATE DIRECTORY imgdir AS 'C:\DBimages\';
+-- Una vez culminada lo anterior ya tienen la ruta registrada y pueden colocar las imagenes a usar
+-- DROP DIRECTORY IMGDIR;
