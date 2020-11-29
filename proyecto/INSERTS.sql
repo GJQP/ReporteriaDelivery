090dd339123ec2dd5098cb2f19499e3b09bb2708
IxDevel <56054255+IxDevel@users.noreply.github.com>
2020-11-29 00:26:43 -0400

    Update INSERTS.sql

--- proyecto/INSERTS.sql
+++ proyecto/INSERTS.sql
@@ -1,1 +1,25 @@
-INSERT INTO estados (NOMBRE) VALUES ('Amazonas');
+INSERT INTO estados VALUES (DEFAULT, 'Amazonas'); --1
+INSERT INTO estados VALUES (DEFAULT, 'Anzoátegui'); --2
+INSERT INTO estados VALUES (DEFAULT, 'Apure'); --3
+INSERT INTO estados VALUES (DEFAULT, 'Aragua'); --4
+INSERT INTO estados VALUES (DEFAULT, 'Barinas'); --5
+INSERT INTO estados VALUES (DEFAULT, 'Bolívar'); --6
+INSERT INTO estados VALUES (DEFAULT, 'Carabobo'); --7
+INSERT INTO estados VALUES (DEFAULT, 'Cojedes'); --8
+INSERT INTO estados VALUES (DEFAULT, 'Delta Amacuro'); --9
+INSERT INTO estados VALUES (DEFAULT, 'Delta Amacuro'); --10
+INSERT INTO estados VALUES (DEFAULT, 'Distrito Capital'); --11
+INSERT INTO estados VALUES (DEFAULT, 'Falcón'); --12
+INSERT INTO estados VALUES (DEFAULT, 'Guárico'); --13
+INSERT INTO estados VALUES (DEFAULT, 'Lara'); --14
+INSERT INTO estados VALUES (DEFAULT, 'Mérida'); --15
+INSERT INTO estados VALUES (DEFAULT, 'Miranda'); --16
+INSERT INTO estados VALUES (DEFAULT, 'Monagas'); --17
+INSERT INTO estados VALUES (DEFAULT, 'Nueva Esparta'); --18
+INSERT INTO estados VALUES (DEFAULT, 'Portuguesa'); --19
+INSERT INTO estados VALUES (DEFAULT, 'Sucre'); --20
+INSERT INTO estados VALUES (DEFAULT, 'Táchira'); --21
+INSERT INTO estados VALUES (DEFAULT, 'Vargas'); --22
+INSERT INTO estados VALUES (DEFAULT, 'Yaracuy'); --23
+INSERT INTO estados VALUES (DEFAULT, 'Zulia'); --24
+INSERT INTO estados VALUES (DEFAULT, 'Dependencias Federales'); --25
