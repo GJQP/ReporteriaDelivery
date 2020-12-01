8bfc3cfedf8e57a57ade44d5aca61d6b911896fe
Gustavo Quintana <gjquintanap@gmail.com>
2020-12-01 15:37:08 -0400

    Correcciones en Estados

--- proyecto/INSERTS.sql
+++ proyecto/INSERTS.sql
@@ -7,21 +7,21 @@ INSERT INTO estados VALUES (DEFAULT, 'Bolívar'); --6
 INSERT INTO estados VALUES (DEFAULT, 'Carabobo'); --7
 INSERT INTO estados VALUES (DEFAULT, 'Cojedes'); --8
 INSERT INTO estados VALUES (DEFAULT, 'Delta Amacuro'); --9
-INSERT INTO estados VALUES (DEFAULT, 'Delta Amacuro'); --10
-INSERT INTO estados VALUES (DEFAULT, 'Distrito Capital'); --11
-INSERT INTO estados VALUES (DEFAULT, 'Falcón'); --12
-INSERT INTO estados VALUES (DEFAULT, 'Guárico'); --13
-INSERT INTO estados VALUES (DEFAULT, 'Lara'); --14
-INSERT INTO estados VALUES (DEFAULT, 'Mérida'); --15
-INSERT INTO estados VALUES (DEFAULT, 'Miranda'); --16
-INSERT INTO estados VALUES (DEFAULT, 'Monagas'); --17
-INSERT INTO estados VALUES (DEFAULT, 'Nueva Esparta'); --18
-INSERT INTO estados VALUES (DEFAULT, 'Portuguesa'); --19
-INSERT INTO estados VALUES (DEFAULT, 'Sucre'); --20
-INSERT INTO estados VALUES (DEFAULT, 'Táchira'); --21
-INSERT INTO estados VALUES (DEFAULT, 'Vargas'); --22
-INSERT INTO estados VALUES (DEFAULT, 'Yaracuy'); --23
-INSERT INTO estados VALUES (DEFAULT, 'Zulia'); --24
+INSERT INTO estados VALUES (DEFAULT, 'Falcón'); --10
+INSERT INTO estados VALUES (DEFAULT, 'Guárico'); --11
+INSERT INTO estados VALUES (DEFAULT, 'Lara'); --12
+INSERT INTO estados VALUES (DEFAULT, 'Mérida'); --13
+INSERT INTO estados VALUES (DEFAULT, 'Miranda'); --14
+INSERT INTO estados VALUES (DEFAULT, 'Monagas'); --15
+INSERT INTO estados VALUES (DEFAULT, 'Nueva Esparta'); --16
+INSERT INTO estados VALUES (DEFAULT, 'Portuguesa'); --17
+INSERT INTO estados VALUES (DEFAULT, 'Sucre'); --18
+INSERT INTO estados VALUES (DEFAULT, 'Táchira'); --19
+INSERT INTO estados VALUES (DEFAULT, 'Trujillo'); --20
+INSERT INTO estados VALUES (DEFAULT, 'Vargas'); --21
+INSERT INTO estados VALUES (DEFAULT, 'Yaracuy'); --22
+INSERT INTO estados VALUES (DEFAULT, 'Zulia'); --23
+INSERT INTO estados VALUES (DEFAULT, 'Distrito Capital'); --24
 INSERT INTO estados VALUES (DEFAULT, 'Dependencias Federales'); --25
 
 --MUNICIPIOS
--- proyecto/VIEWS.sql
+++ proyecto/VIEWS.sql
@@ -1,1 +1,7 @@
---CREATE OR REPLACE VIEW zonas AS
+/*
+CREATE OR REPLACE VIEW ubicaciones(id_zona,zona,municipio,estado,id_municipio,id_estado) AS
+SELECT z.id_estado,z.id_municipio,z.id, z.nombre, m.nombre, e.nombre FROM estados e
+JOIN municipios m ON e.id = m.id_estado
+JOIN zonas z ON m.id_estado = z.id_estado AND m.id = z.id_municipio
+ORDER BY id_estado
+*/
