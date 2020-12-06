b9fa357a48d42c64dd8d0475faead204bb867ffb
IxDevel <56054255+IxDevel@users.noreply.github.com>
2020-12-06 19:52:34 -0400

    Insert Creados + TDA Ubicación listo

--- proyecto/TDAS.sql
+++ proyecto/TDAS.sql
@@ -136,16 +136,106 @@ CREATE OR REPLACE TYPE BODY CANCELACION IS
 END;
 /
 CREATE OR REPLACE TYPE UBICACION AS OBJECT (
-    coordenada SDO_GEOMETRY,
+    latitud NUMBER(17,14),
+    longitud NUMBER(17,14),
     actualizado TIMESTAMP,
-    MEMBER FUNCTION OBTENER_POSICION(timpo_transcurrido TIMESTAMP, velocidad_med NUMBER) RETURN SDO_GEOMETRY
+    STATIC FUNCTION OBTENER_POSICION(p2 UBICACION, p1 UBICACION, velocidad_med NUMBER, tiempo_horas NUMBER) RETURN UBICACION,
+    STATIC FUNCTION VALIDAR_LATITUD(lat NUMBER) RETURN NUMBER,
+    STATIC FUNCTION VALIDAR_LONGITUD(lon NUMBER) RETURN NUMBER,
+    STATIC FUNCTION SUMAR_COORDENADAS(p1 UBICACION, p2 UBICACION) RETURN UBICACION,
+    STATIC FUNCTION MODULO(p UBICACION) RETURN NUMBER,
+    STATIC FUNCTION DISTANCIA_ENTRE_DOS_PUNTOS(p2 UBICACION, p1 UBICACION) RETURN NUMBER,
+    STATIC FUNCTION VECTOR_UBICACION(p2 UBICACION, p1 UBICACION) RETURN UBICACION,
+    STATIC FUNCTION OBTENER_TIEMPO_ESTIMADO_EN_HORAS(p2 UBICACION, p1 UBICACION, velocidad_med NUMBER) RETURN NUMBER
 );
 /
 CREATE OR REPLACE TYPE BODY UBICACION IS
-    MEMBER FUNCTION OBTENER_POSICION(timpo_transcurrido TIMESTAMP, velocidad_med NUMBER) RETURN SDO_GEOMETRY IS
+    STATIC FUNCTION OBTENER_POSICION(p2 UBICACION, p1 UBICACION, velocidad_med NUMBER, tiempo_horas NUMBER) RETURN UBICACION IS
+        modulo_deseado NUMBER(30,14);
+        modulo_real NUMBER(30,14);
+        vector_ubi UBICACION;
+        modulo_kmh NUMBER(30,14);
+        tiempo_necesario NUMBER(30,14);
+        delta NUMBER(30,14);
     BEGIN
-        RETURN NULL;
+        delta := 0.000002;
+        modulo_kmh := 0.00134245661127421263782387653989*velocidad_med;
+        modulo_deseado := UBICACION.DISTANCIA_ENTRE_DOS_PUNTOS(p2, p1);
+        vector_ubi := UBICACION.VECTOR_UBICACION(p2, p1);
+        modulo_real := UBICACION.DISTANCIA_ENTRE_DOS_PUNTOS(UBICACION(vector_ubi.latitud*modulo_kmh*tiempo_horas + p1.latitud, vector_ubi.longitud*modulo_kmh*tiempo_horas + p1.longitud, SYSDATE), p1);
+        DBMS_OUTPUT.PUT_LINE(modulo_real - modulo_deseado);
+        IF (modulo_real > modulo_deseado + delta) THEN
+            DBMS_OUTPUT.PUT_LINE('El vehículo recorre más de lo esperado' || (modulo_real-modulo_deseado)/modulo_kmh);
+            tiempo_necesario := DISTANCIA_ENTRE_DOS_PUNTOS(p2,p1)/modulo_kmh;
+            RETURN UBICACION(p2.latitud, p2.longitud, p1.ACTUALIZADO+tiempo_necesario/24);
+        ELSIF (modulo_real < (modulo_deseado + delta) AND modulo_real > (modulo_deseado - delta)) THEN --Falta agregar un delta
+            DBMS_OUTPUT.PUT_LINE('El vehículo recorre lo esperado' || to_char(tiempo_horas, '99.999999'));
+            RETURN UBICACION(p2.latitud, p2.longitud, p1.ACTUALIZADO+tiempo_horas/24);
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('El vehículo recorre menos de lo esperado' || to_char(tiempo_horas, '99.999999'));
+            RETURN UBICACION(vector_ubi.latitud*modulo_kmh*tiempo_horas + p1.latitud, vector_ubi.longitud*modulo_kmh*tiempo_horas + p1.longitud, p1.ACTUALIZADO+tiempo_horas/24);
+        END IF;
+    END;
+
+    STATIC FUNCTION VALIDAR_LATITUD(lat NUMBER) RETURN NUMBER IS
+    BEGIN
+        IF (lat > 90 OR lat < -90) THEN
+             RAISE_APPLICATION_ERROR(-20001,'Error: la latitud debe estar entre 90 y -90');
+        ELSE
+            RETURN lat;
+        END IF;
+    END;
+
+    STATIC FUNCTION VALIDAR_LONGITUD(lon NUMBER) RETURN NUMBER IS
+    BEGIN
+        IF (lon > 180 OR lon < -180) THEN
+             RAISE_APPLICATION_ERROR(-20001,'Error: la longitud debe estar entre 180 y -180');
+        ELSE
+            RETURN lon;
+        END IF;
+    END;
+
+    STATIC FUNCTION SUMAR_COORDENADAS(p1 UBICACION, p2 UBICACION) RETURN UBICACION IS
+    BEGIN
+        RETURN UBICACION(p1.latitud + p2.latitud, p1.longitud + p2.longitud, SYSDATE);
+    END;
+
+    STATIC FUNCTION MODULO(p UBICACION) RETURN NUMBER IS
+    BEGIN
+        RETURN SQRT(p.latitud*p.latitud + p.longitud*p.longitud);
+    END;
+
+    STATIC FUNCTION DISTANCIA_ENTRE_DOS_PUNTOS(p2 UBICACION, p1 UBICACION) RETURN NUMBER IS
+    BEGIN
+        RETURN MODULO(UBICACION(p2.latitud - p1.latitud, p2.longitud - p1.longitud, SYSDATE));
+    END;
+
+    STATIC FUNCTION VECTOR_UBICACION(p2 UBICACION, p1 UBICACION) RETURN UBICACION IS
+    modulo NUMBER(30,14);
+    BEGIN
+        modulo := UBICACION.DISTANCIA_ENTRE_DOS_PUNTOS(p2, p1);
+        RETURN UBICACION((p2.latitud - p1.latitud)/modulo, (p2.longitud - p1.longitud)/modulo, SYSDATE);
+    END;
+
+    STATIC FUNCTION OBTENER_TIEMPO_ESTIMADO_EN_HORAS(p2 UBICACION, p1 UBICACION, velocidad_med NUMBER) RETURN NUMBER IS
+        modulo_kmh NUMBER(30,14);
+        delta NUMBER(30,14);
+    BEGIN
+        delta := 0.00004;
+        modulo_kmh := 0.00134245661127421263782387653989*velocidad_med;
+        RETURN DISTANCIA_ENTRE_DOS_PUNTOS(p2,p1)/modulo_kmh + delta;
     END;
 END;
 /
 
+--SELECT UBICACION(10.498337827572668, -66.88071887446146, SYSDATE) FROM DUAL;
+--SELECT UBICACION(10.5022515698636, -66.87855164952926, SYSDATE) FROM DUAL;
+--SELECT UBICACION.OBTENER_POSICION(UBICACION(10.5022515698636, -66.87855164952926, SYSDATE), UBICACION(10.498337827572668, -66.88071887446146, SYSDATE), 40,5/60) FROM DUAL;
+--SELECT UBICACION.VECTOR_UBICACION(UBICACION(10.5022515698636, -66.87855164952926, SYSDATE),UBICACION(10.498337827572668, -66.88071887446146, SYSDATE)) FROM DUAL;
+--SELECT ROUND(UBICACION.OBTENER_TIEMPO_ESTIMADO_EN_HORAS(UBICACION(10.5022515698636, -66.87855164952926, SYSDATE), UBICACION(10.498337827572668, -66.88071887446146, SYSDATE), 40)*60) MINUTOS FROM DUAL;
+--SELECT 5-4.99874037800506796104299661316991687011 FROM DUAL;
+-- P1
+--10.498337827572668, -66.88071887446146
+-- P2
+--10.5022515698636, -66.87855164952926
+-- Se supone que debe ir de P1 a P2 en 5 min a 40Km/h
