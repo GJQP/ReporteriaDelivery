795953c105db776607224dacd5858f8892a33043
MV Ortiz <mariatua2@gmail.com>
2021-02-09 00:04:46 -0400

    privilegios de tablas

--- proyecto/ROLES_SYSTEM.sql
+++ proyecto/ROLES_SYSTEM.sql
@@ -1,4 +1,8 @@
 -- ROLES
 CREATE ROLE c##Transportista_ROL;
+CREATE ROLE c##AppDelivery_ROL;
+CREATE ROLE c##Sucursal_ROL;
+CREATE ROLE c##Empresa_ROL;
+CREATE ROLE c##UsuarioFinal_ROL;
 
 
--- proyecto/USERS_SYSTEM.sql
+++ proyecto/USERS_SYSTEM.sql
@@ -9,3 +9,11 @@ create user c##Transportista2 identified by password;
 GRANT CONNECT, RESOURCE TO c##Transportista2;
 GRANT c##Transportista_ROL TO c##Transportista2;
 
+--cuentas appdelivery
+create user c##AppDelivery1 identified by password;
+GRANT CONNECT, RESOURCE TO c##AppDelivery1;
+GRANT c##AppDelivery_ROL TO c##AppDelivery1;
+
+create user c##AppDelivery2 identified by password;
+GRANT CONNECT, RESOURCE TO c##AppDelivery2;
+GRANT c##AppDelivery_ROL TO c##AppDelivery2;
